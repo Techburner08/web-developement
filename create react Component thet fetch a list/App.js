bad29534387cf91ace28cd11c7eb9026a3040529
Tech Burner <158762489+Techburner08@users.noreply.github.com>
2025-06-19 23:14:23 +0530

    Add files via upload

---
+++ create react Component thet fetch a list/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserList from './UserList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>User List</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
