14d851869ec19c515344d0a9dad8c7bab5ea4664
Tech Burner <158762489+Techburner08@users.noreply.github.com>
2025-06-19 22:53:02 +0530

    Create hi.c

---
+++ create react Component thet fetch a list/hi.c
@@ -0,0 +1,3 @@
+// Q8. Create a React component that fetches a list of users from the API
+// https://jsonplaceholder.typicode.com/users using axios and displays their names and email addresses
+// in a list.
