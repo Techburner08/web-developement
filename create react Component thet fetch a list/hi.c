028b8d7ea7b435da4d4360bf528bf005c8545da3
Tech Burner <158762489+Techburner08@users.noreply.github.com>
2025-06-19 23:14:45 +0530

    Delete create react Component thet fetch a list/hi.c

--- create react Component thet fetch a list/hi.c
+++
@@ -1,3 +0,0 @@
-// Q8. Create a React component that fetches a list of users from the API
-// https://jsonplaceholder.typicode.com/users using axios and displays their names and email addresses
-// in a list.
